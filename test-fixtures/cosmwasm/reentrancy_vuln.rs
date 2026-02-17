f2a1424ef3fd3edbcf6bee7182dea84554b08b0b
dehvCurtis <dehvCurtis@users.noreply.github.com>
2026-02-17 00:20:53 -0700

    Update test fixtures to match new FP filters
    SOL-010 fixture: Use non-global PDA seeds (escrow, reward_pool) instead
    of config/vault which are now recognized as intentionally global.
    CW-002 fixture: Use IBC/reply handlers instead of plain execute functions
    since non-IBC handlers are no longer flagged.
    
    Maintains 50/50 detector coverage with 159 total findings.

--- test-fixtures/cosmwasm/reentrancy_vuln.rs
+++ test-fixtures/cosmwasm/reentrancy_vuln.rs
@@ -1,38 +1,41 @@
 // Test fixture for CW-002: cosmwasm-reentrancy
-// Storage writes followed by add_message() (CEI violation)
+// Storage writes followed by add_message() in IBC/reply context (CEI violation)
+// CosmWasm is non-reentrant by design, but IBC hooks and reply handlers can be exploited
 
-fn execute_transfer(
+fn ibc_packet_receive(
     deps: DepsMut,
-    info: MessageInfo,
-    recipient: String,
+    env: Env,
+    msg: IbcPacketReceiveMsg,
     amount: Uint128,
 ) -> StdResult<Response> {
-    // State update BEFORE external call
-    BALANCES.save(deps.storage, &info.sender, &(balance - amount))?;
+    // State update BEFORE external call in IBC handler
+    let new_balance = old_balance + amount;
+    BALANCES.save(deps.storage, &msg.packet.dest.channel_id, &new_balance)?;
 
-    let msg = WasmMsg::Execute {
+    let transfer_msg = WasmMsg::Execute {
         contract_addr: recipient.clone(),
         msg: to_binary(&TransferMsg { amount })?,
         funds: vec![],
     };
 
     Ok(Response::new()
-        .add_message(msg)
-        .add_attribute("action", "transfer"))
+        .add_message(transfer_msg)
+        .add_attribute("action", "ibc_receive"))
 }
 
-fn execute_swap(
+fn reply_handler(
     deps: DepsMut,
-    info: MessageInfo,
-    pool: String,
+    env: Env,
+    msg: Reply,
 ) -> StdResult<Response> {
+    // State update in reply before dispatching another SubMsg
     STATE.save(deps.storage, &new_state)?;
 
-    let swap_msg = CosmosMsg::Wasm(WasmMsg::Execute {
+    let followup = CosmosMsg::Wasm(WasmMsg::Execute {
         contract_addr: pool,
         msg: to_binary(&SwapMsg {})?,
-        funds: info.funds,
+        funds: vec![],
     });
 
-    Ok(Response::new().add_submessage(SubMsg::new(swap_msg)))
+    Ok(Response::new().add_submessage(SubMsg::new(followup)))
 }
